ebdd3cbb9384869b6f11c7eecadc5c74d14bf4bf
Manuel Lopez <122247515+Thelegend-ml@users.noreply.github.com>
2023-03-27 23:21:54 -0700

    Everything works perfectly :)

--- script.js
+++ script.js
@@ -136,15 +136,17 @@ Array.from(aboutMeTextContent).forEach((char) => {
 // End of About Me Text 
 
 // Projects
-const projects = document.querySellectorAll(".project");
+const projects = document.querySelectorAll('.project')
 
-projects.forEach((project) => {
-    project.addEventListener("mouseenter", () => {
-            project.firstElementChild.style.top = `-${
-                project.firstElementChild.offsetHeight - project.
-                    offsetHeight + 20
-                }px`;
-        });
+projects.forEach(project => {
+    project.addEventListener('mouseenter', () => {
+        project.firstElementChild.style.top = `-${project.firstElementChild.offsetHeight - project.offsetHeight + 20 }px`;
+
+    });
+
+    project.addEventListener('mouseleave', () => {
+        project.firstElementChild.style.top = "2rem";
+    })
 });
 // End of Prtojects 
 
