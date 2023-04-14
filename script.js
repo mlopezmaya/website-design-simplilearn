7047ae08adb64f42615b2bdd6b54411176cd5d87
Manuel Lopez <122247515+Thelegend-ml@users.noreply.github.com>
2023-04-13 23:04:53 -0700

    getting to the end of building website design!

--- script.js
+++ script.js
@@ -118,6 +118,26 @@ mainBtns.forEach(btn => {
 
 // End of Main Butoon 
 
+// Navigation 
+const menuIcon = document.querySelector(".menu-icon");
+const navbar = document.querySelector(".navbar");
+
+document.addEventListener("scroll", () => {
+    menuIcon.classList.add("show-menu-icon");
+    navbar.classList.add("hide-navbar");
+
+    if(window.scrollY === 0) {
+        menuIcon.classList.remove("show-menu-icon");
+        navbar.classList.remove("hide-navbar");
+    }
+});
+
+menuIcon.addEventListener("click", () => {
+    menuIcon.classList.remove("show-menu-icon");
+    navbar.classList.remove("hide-navbar");
+});
+// End of Navigation 
+
 
 // About Me Text 
 const aboutMeText = document.querySelector(".about-me-text");
@@ -176,4 +196,39 @@ projects.forEach(project => {
 
 });
 // End of Prtojects 
+// End of Form
+
+// Slideshow
+const slideshow = document.querySelector(".slideshow");
+
+setInterval(() => {
+    const firstIcon = slideshow.firstElementChild;
+
+    firstIcon.classList.add("faded-out");
+
+    const thirdIcon = slideshow.children[3]
+
+    thirdIcon.classList.add("light");
+
+    thirdIcon.previousElementSibling.classList.remove("light");
+
+    setTimeout(() => {
+        slideshow.removeChild(firstIcon);
+
+        slideshow.appendChild(firstIcon);
+
+
+        setTimeout(() => {
+             firstIcon.classList.remove("faded-out");
+        }, 500);
+
+    }, 500);
+}, 3000);
+
+
+
+
+
+// End of Slideshow
+// End of Section 5
 
