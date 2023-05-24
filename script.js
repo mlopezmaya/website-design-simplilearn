de97e8fcb30e75d219a7be42006bcec214912eff
Manuel Lopez <122247515+mlopezmaya@users.noreply.github.com>
2023-05-24 15:18:55 -0700

    05/24/2023

--- script.js
+++ script.js
@@ -245,14 +245,6 @@ document.querySelectorAll(".service-btn").forEach((service) => {
 
 // Section 5
 // Form 
-const checkLength = (input, min) => {
-    if(input.value.trim().length < min) {
-        error(input, `${input.id} must be at least ${min}characters`);
-    } else {
-        success(input);
-    }
-};
-
 const checkEmail = (input) => {
 const regEx = /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i;
 
@@ -288,20 +280,35 @@ const error = (input, message) => {
     input.nextElementSibling.textContent = message;
 };
 
+const success = (input) => {
+    input.nextElementSibling.classList.remove("error");
+};
+
 const checkRequiredFields = (inputArr) => {
 inputArr.forEach(input => {
     if(input.value.trim() === "") {
         error(input, `${input.id} is required`);
+    } else {
+    success(input); 
     }
     });
 };
 
-form.addEventListener("submit", e => {
-    e.preventDefault()
+const checkLength = (input, min) => {
+    if(input.value.trim().length < min) {
+        error(input, `${input.id} must be at least ${min} characters`);
+    } else {
+        success(input);
+
+    }
+};
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
 
     checkRequiredFields([username, email, subject, message]);
 
-})
+});
 
 // End of Form Validation 
 
